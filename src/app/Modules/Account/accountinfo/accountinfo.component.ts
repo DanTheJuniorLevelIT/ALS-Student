7af7f9934e2af9206c6955b90b291a15aa5131b7
John Lloyd Glee <johnlloydglee26@gmail.com>
2024-09-24 09:02:05 +0800

    awit

--- src/app/Modules/Account/accountinfo/accountinfo.component.ts
+++ src/app/Modules/Account/accountinfo/accountinfo.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../../../student.service';
 
 @Component({
   selector: 'app-accountinfo',
@@ -7,6 +8,28 @@ import { Component } from '@angular/core';
   templateUrl: './accountinfo.component.html',
   styleUrl: './accountinfo.component.css'
 })
-export class AccountinfoComponent {
+export class AccountinfoComponent implements OnInit{
+  learner: any;
+
+  constructor(private studentservice: StudentService) { 
+  }
+  ngOnInit(): void {
+    const token = localStorage.getItem('authToken'); // Retrieve Token from localStorage
+  
+    if (token) {
+      this.studentservice.getLearnerByToken(token).subscribe({
+        next: (data) => {
+          this.learner = data;
+          // Assuming the LRN is a property of the returned data
+          
+        },
+        error: (err) => {
+          console.error('Error fetching learner data', err);
+        }
+      });
+    } else {
+      console.error('No Token Found. User is not authenticated');
+    }
+  }
 
 }
